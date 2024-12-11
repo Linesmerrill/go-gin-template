8a1ba06c7e24974e4b9e6f1fcea46ebf3dd5f0c0
Merrill Lines <linesmerrill@gmail.com>
2024-12-11 07:22:26 -0700

    Update import for project; ignore vendor

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/webgoprojects/controllers"
-	"github.com/webgoprojects/middleware"
+	"github.com/webgoprojects/go-gin-template/controllers"
+	"github.com/webgoprojects/go-gin-template/middleware"
 )
 
 func main() {
