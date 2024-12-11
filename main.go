33211c00c1fa097ff3f6946a117016aef70d5774
Merrill Lines <linesmerrill@gmail.com>
2024-12-11 07:17:23 -0700

    Update import statments to correctly show the path

--- main.go
+++ main.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-        "github.com/gin-gonic/gin"
-        "your-project-name/controllers"
-        "your-project-name/middleware"
+	"github.com/gin-gonic/gin"
+	"github.com/webgoprojects/controllers"
+	"github.com/webgoprojects/middleware"
 )
 
 func main() {
-        r := gin.Default()
+	r := gin.Default()
 
-        // Middleware
-        r.Use(middleware.Logger())
+	// Middleware
+	r.Use(middleware.Logger())
 
-        // Routes
-        r.GET("/", controllers.Index)
-        r.GET("/about", controllers.About)
+	// Routes
+	r.GET("/", controllers.Index)
+	r.GET("/about", controllers.About)
 
-        // Serve static files
-        r.Static("/static", "./static")
+	// Serve static files
+	r.Static("/static", "./static")
 
-        // Start the server
-        r.Run(":8080")
+	// Start the server
+	r.Run(":8080")
 }
