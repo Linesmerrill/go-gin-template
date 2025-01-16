a8dee6a0f1c82790ebd8e398e5058ebe06e5c489
Merrill Lines <linesmerrill@gmail.com>
2025-01-15 17:22:19 -0700

    Update import statement

--- routers/router.go
+++ routers/router.go
@@ -2,7 +2,7 @@ package routers
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/linesmerrill/go-gin-template/controllers"
+	"github.com/webgoprojects/go-gin-template/controllers"
 )
 
 func SetupRouter() *gin.Engine {
